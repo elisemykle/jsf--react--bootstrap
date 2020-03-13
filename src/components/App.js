3500671b53737cd7e572343207071ac611fc566b
Kingsley <k@n.com>
2020-03-13 10:06:51 +0100

    removed working code

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
 
 export default function App() {
     return (
         <div className="App">
-            <Navbar bg="primary" variant="dark" expand="lg">
-                <Navbar.Brand href="/">Website Name</Navbar.Brand>
-                <Navbar.Toggle aria-controls="mainNav" />
-                <Navbar.Collapse id="mainNav">
-                    <Nav>
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/section1/">Section 1</Nav.Link>
-                        <Nav.Link href="/section2/">Page 2</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/section1/">Section 1</a>
+                <a href="/section2/">Page 2</a>
+            </nav>
         </div>
     );
 }
