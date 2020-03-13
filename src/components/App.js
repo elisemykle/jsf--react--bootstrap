c7da20c1eefd460c6ec917492fde1ce659f4f075
Kingsley <k@n.com>
2020-03-13 09:21:02 +0100

    added working code

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Noroff Task</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar bg="primary" variant="dark" expand="lg">
+                <Navbar.Brand href="/">Website Name</Navbar.Brand>
+                <Navbar.Toggle aria-controls="mainNav" />
+                <Navbar.Collapse id="mainNav">
+                    <Nav>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/section1/">Section 1</Nav.Link>
+                        <Nav.Link href="/section2/">Page 2</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
 }
